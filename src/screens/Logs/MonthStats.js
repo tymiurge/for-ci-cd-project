4ce1049411d6438cf26942ce9e6cb3367831c6bc
tymiurge <tmrlabas@gmail.com>
2018-10-08 23:18:19 +0300

    some UI refactorings in Logs screen

--- src/screens/Logs/MonthStats.js
+++ src/screens/Logs/MonthStats.js
@@ -5,20 +5,15 @@ import { layouting } from 'components'
 const MonthStats = ({incomes, outcomes, savings}) => {
     const max = Math.max.apply(null, [incomes, outcomes, savings])
     return (
-        <div>
-        <Message
-            attached
-            size='mini'
-            header='Oct 2018 statistics'
-      />
-        <Segment attached style={{marginBottom: '1em'}}>
-            <layouting.RowsStack>
-            <Progress percent={incomes / max * 100} size='tiny' color='blue'>Incomes: {incomes}</Progress>
-            <Progress percent={outcomes / max * 100} size='tiny' color='red'>Outcomes: {outcomes}</Progress>
-            <Progress percent={savings / max * 100} size='tiny' color='green'>Savings: {savings}</Progress>
-            </layouting.RowsStack>
-        </Segment>
+        
+        <div style={{marginTop: '0.5em', marginBottom: '1.5em'}}>
+        <layouting.RowsStack>
+        <Progress percent={incomes / max * 100} size='tiny' color='blue'>Incomes: {incomes}</Progress>
+        <Progress percent={outcomes / max * 100} size='tiny' color='red'>Outcomes: {outcomes}</Progress>
+        <Progress percent={savings / max * 100} size='tiny' color='green'>Savings: {savings}</Progress>
+        </layouting.RowsStack>
         </div>
+        
     )
 }
 
