0a9ab6990bd2714709da1bf87dd695b591dfc2c0
tymiurge <tmrlabas@gmail.com>
2018-10-09 13:32:17 +0300

    adding functionality to the screens/Logs/Toolbar/Filter

--- src/screens/Logs/Toolbar/Filter.js
+++ src/screens/Logs/Toolbar/Filter.js
@@ -9,37 +9,69 @@ class Filter extends React.Component {
         onClose: PropTypes.func.isRequired
     }
 
+    state = {
+        messageVisible: false,
+        value: ''
+    }
+
+    toggleMessageVisibility = () => {
+        this.setState({...this.state, messageVisible: !this.state.messageVisible})
+    }
+
     render() {
         return (
             <Segment>
                 <Input
-                fluid 
-                action={(
-                    <Button.Group>
-                        <Button icon='close' color='red' />
-                        <Button icon='question' color='blue' />
-                    </Button.Group>
-                )} 
-                placeholder='Search...' />
+                    fluid 
+                    action={(
+                        <Button.Group>
+                            <Button
+                                icon='close'
+                                color='red'
+                                onClick={() => this.props.onClose()}
+                            />
+                            <Button
+                                icon='question' 
+                                color='blue' 
+                                onClick={() => this.toggleMessageVisibility()}
+                            />
+                        </Button.Group>
+                    )} 
+                    placeholder='Search...'
+                    value={this.state.value}
+                    onChange={e => {
+                        const newValue = e.target.value
+                        this.setState(
+                            {...this.state, value: newValue},
+                            () => this.props.onChange(newValue)
+                        )
+                    }}
+                />
+                {
+                    this.state.messageVisible &&
+                    <Message>
+                        <p>
+                        basicaly the sintacsis of filtering/searching is: <br/>
+                        {'<field> <operator> <criteria>'}
+                        </p>
 
-                <Message>
-                    <p>
-                    basicaly the sintacsis of filtering/searching is: <br/>
-                    {'<field> <operator> <criteria>'}
-                    </p>
+                        <p>
+                            For fields Type, Name, Tag and From two operations are supported: ':' and '='<br/>
+                            In first case entries will be filtered by 'has the criteria' rule, in case of using '=' the 
+                            entries will be filtered by 'equals to the criteria'.
+                        </p>
 
-                    <p>
-                        For fields Type, Name, Tag and From two operations are supported: ':' and '='<br/>
-                        In first case entries will be filtered by 'has the criteria' rule, in case of using '=' the 
-                        entries will be filtered by 'equals to the criteria'.
-                    </p>
+                        <p>
+                            For fields Amount and Date the following operations are supported: {"'=', '>', '<', '>=', '<='"}
+                        </p>
 
-                    <p>
-                        For fields Amount and Date the following operations are supported: {"'=', '>', '<', '>=', '<='"}
-                    </p>
+                        <Button 
+                            content='Got it'
+                            onClick={() => this.toggleMessageVisibility()}
+                        />
+                    </Message>
+                }
 
-                    <Button>Got it</Button>
-                </Message>
             </Segment>
         )
     }
