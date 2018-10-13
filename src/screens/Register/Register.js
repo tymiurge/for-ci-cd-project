304a665967f903c459aa0083318e50651a5cd387
kaa <tmrlabas@gmail.com>
2018-10-13 17:48:09 +0300

    adding password equality validation into Registration form

--- src/screens/Register/Register.js
+++ src/screens/Register/Register.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Grid, Header, Form, Segment, Button } from 'semantic-ui-react'
+import { Grid, Header, Form, Segment, Button, Input, Message } from 'semantic-ui-react'
+import { controls } from 'components'
+import { validation } from 'utils'
 
 class Register extends React.Component {
 
-    render() {
-        return (
-            <div className='login-form'>
+  state = {
+    firstName: '',
+    lastName: '',
+    mail: '',
+    pswd: '',
+    pswdDuplication: '',
+    pswdMatch: true
+  }
+
+  onFieldChange = (value, fieldName) => {
+    this.setState({...this.state, [fieldName]: value})
+  }
+
+  onSubmit = () => {
+    const { pswd, pswdDuplication } = this.state
+    if (pswd !== pswdDuplication) {
+      this.setState({...this.state, pswdMatch: false})
+    }
+  }
+
+  render() {
+    
+    return (
+      <div className='login-form'>
         {/*
           Heads up! The styles below are necessary for the correct render of this example.
           You can do same with CSS, the main idea is that all the elements up to the `Grid`
@@ -26,66 +49,79 @@ class Register extends React.Component {
             <Form size='large'>
               <Segment>
               <Form.Group widths='equal'>
-                    <Form.Input 
-                        fluid 
-                        icon='user' 
-                        iconPosition='left' 
-                        placeholder='First Name'
-                        //value={this.state.login}
-                        //onChange={ e => this.setState({...this.state, login: e.target.value})}
-                        />
-                    <Form.Input 
-                        fluid 
-                        icon='user' 
-                        iconPosition='left' 
-                        placeholder='Last Name'
-                        //value={this.state.login}
-                        //onChange={ e => this.setState({...this.state, login: e.target.value})}
-                        />
-                </Form.Group>
-                <Form.Input 
-                  fluid 
-                  icon='mail' 
-                  iconPosition='left' 
-                  placeholder='E-mail address'
-                  //value={this.state.login}
-                  //onChange={ e => this.setState({...this.state, login: e.target.value})}
+                <Form.Field>
+                  <controls.ValidatedInput
+                    placeholder='First Name'
+                    icon='user'
+                    fieldBinding='firstName'
+                    onChange={(value, field) => this.onFieldChange(value, field)}
+                    validators={[validation.strIsNotEmpty]}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <controls.ValidatedInput
+                    placeholder='Last Name'
+                    icon='user'
+                    fieldBinding='lastName'
+                    onChange={(value, field) => this.onFieldChange(value, field)}
+                    validators={[validation.strIsNotEmpty]}
+                  />
+                </Form.Field>
+              </Form.Group>
+              <Form.Field>
+                <controls.ValidatedInput
+                  placeholder='Mail'
+                  icon='mail'
+                  type='email'
+                  fieldBinding='mail'
+                  onChange={(value, field) => this.onFieldChange(value, field)}
+                  validators={[validation.strIsNotEmpty]}
                 />
-                <Form.Input
-                  fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password'
-                  type='password'
-                  //value={this.state.password}
-                  //onChange={ e => this.setState({...this.state, password: e.target.value})}
-                />
-                <Form.Input
-                  fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password one more time'
-                  type='password'
-                  //value={this.state.password}
-                  //onChange={ e => this.setState({...this.state, password: e.target.value})}
-                />
-
-                <Button
-                  color='blue'
-                  fluid 
-                  size='large'
-                  content='Register'
-                  onClick={() => this.props.onSubmit(this.state.login, this.state.password)}
-                />
-                
-              </Segment>
-            </Form>
+              </Form.Field>
+              <Form.Group widths='equal'>
+                <Form.Field>
+                  <controls.ValidatedInput
+                    placeholder='Password'
+                    icon='lock'
+                    type='password'
+                    fieldBinding='pswd'
+                    onChange={(value, field) => this.onFieldChange(value, field)}
+                    validators={[validation.strIsNotEmpty]}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <controls.ValidatedInput
+                    placeholder='Repeat password'
+                    icon='lock'
+                    type='password'
+                    fieldBinding='pswdDuplication'
+                    onChange={(value, field) => this.onFieldChange(value, field)}
+                    validators={[validation.strIsNotEmpty]}
+                  />
+                </Form.Field>
+              </Form.Group>  
+              {
+                !this.state.pswdMatch &&
+                <Message negative>
+                  <Message.Header>Passwords do not match</Message.Header>
+                  <p>Please re-enter passswords so that they be equaled to each other.</p>
+                </Message>
+              }
+              <Button
+                color='blue'
+                fluid 
+                size='large'
+                content='Register'
+                onClick={this.onSubmit}
+              />
+            </Segment>
+          </Form>
             
-          </Grid.Column>
-        </Grid>
-      </div>
-        )
-    }
+        </Grid.Column>
+      </Grid>
+    </div>
+  )}
+
 }
 
 export default Register
